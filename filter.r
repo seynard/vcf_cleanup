e5f086ad7aa3f7f5e7b38bb163a4fd4f74c96548
sonia.eynard <your_email@example.com>
2022-10-11 17:10:51 +0200

    correction function list and venn

--- filter.r
+++ filter.r
@@ -1,32 +1,46 @@
 #module load system/R-3.5.1
 #R
 ######## functions ######## 
-installpackages<-function(package_name){if(package_name %in% rownames(installed.packages()) == FALSE) {install.packages(package_name, repos = "http://cran.us.r-project.org")}}
+installpackages<-function(package_name){if(package_name %in% rownames(installed.packages()) == FALSE) {install.packages(package_name)}}
 run_filter<-function(filter_list,filter_name,filter_value,threshold_direction,infile,previous_markers_name,previous_markers){
 	f_list<-list()
-	name_filter<-vector()
 	for(i in 1:length(filter_list)){
 		if(filter_list[i]=='allele'){
 			f_list[[i]]<-paste0(infile$CHROM[nchar(infile$ALT)<=filter_value[filter_name==filter_list[i]] & grepl('\\*',infile$ALT)==F],'_',infile$POS[nchar(infile$ALT)<=filter_value[filter_name==filter_list[i]] & grepl('\\*',infile$ALT)==F])
 		}else if(filter_list[i]%in%filter_name & filter_list[i]!='allele'){
 			if(threshold_direction[filter_name==filter_list[i]]=='below'){
-				name_filter[i]<-paste0(filter_name[filter_name==filter_list[i]],'<',filter_value[filter_name==filter_list[i]])
 				f_list[[i]]<-paste0(infile$CHROM[infile[,filter_list[i]]<filter_value[filter_name==filter_list[i]]],'_',infile$POS[infile[,filter_list[i]]<filter_value[filter_name==filter_list[i]]])
 			}else if (threshold_direction[filter_name==filter_list[i]]=='above'){
 				f_list[[i]]<-paste0(infile$CHROM[infile[,filter_list[i]]>filter_value[filter_name==filter_list[i]]],'_',infile$POS[infile[,filter_list[i]]>filter_value[filter_name==filter_list[i]]])
-				name_filter[i]<-paste0(filter_name[filter_name==filter_list[i]],'>',filter_value[filter_name==filter_list[i]])
 			}
 		}
 	}
 	f_list<-f_list[lapply(f_list,length)>0] 
+	if(previous_markers_name!='none'){f_list[[length(f_list)+1]]<-(previous_markers)}
 	return(f_list)
 }
-plot_venn<-function(filter_list,infile,previous_markers_name,previous_markers){
+name_filter<-function(filter_list,filter_name,filter_value,threshold_direction,infile,previous_markers_name,previous_markers){
+	name_filter<-vector()
+	for(i in 1:length(filter_list)){
+		if(filter_list[i]=='allele'){
+			name_filter[i]<-'allele'
+		}else if(filter_list[i]%in%filter_name & filter_list[i]!='allele'){
+			if(threshold_direction[filter_name==filter_list[i]]=='below'){
+				name_filter[i]<-paste0(filter_name[filter_name==filter_list[i]],'<',filter_value[filter_name==filter_list[i]])
+			}else if (threshold_direction[filter_name==filter_list[i]]=='above'){
+				name_filter[i]<-paste0(filter_name[filter_name==filter_list[i]],'>',filter_value[filter_name==filter_list[i]])
+			}
+		}
+	}
+	if(previous_markers_name!='none'){name_filter<-c(name_filter,previous_markers_name)}
+	return(name_filter)
+}
+plot_venn<-function(filter_list,infile,name_filter,previous_markers_name,previous_markers){
 	f_list<-infile
 	name_filter<-name_filter[!is.na(name_filter)]
 	if(previous_markers_name!='none'){f_list[[length(f_list)+1]]<-(previous_markers)
 		name_filter<-c(name_filter,previous_markers_name)}
-	if('allele'%in%filter_list){name_filter<-c('allele',name_filter)}
+#	if('allele'%in%filter_list){name_filter<-c('allele',name_filter)}
 	myCol<-brewer.pal(length(f_list),"Set2")
 	plot<-venn.diagram(
 		x=f_list,
@@ -81,17 +95,14 @@ for(i in 5:ncol(info)){info[,i][info[,i]=='.']<-0; info[,i]<-as.numeric(info[,i]
 #first venn (mapping quality filter)
 filter_list<-c('FS','SOR','MQ','MQRankSum','ReadPosRankSum')
 f_list1<-run_filter(filter_list,filter_info$filt,filter_info$Filt,filter_info$info,info,'none',NA)
-plot_venn1<-plot_venn(f_list1,info,'none',NA)
 int1<-make_list(f_list1)
 #second venn quality
 filter_list<-c('QUAL','QD')
 f_list2<-run_filter(filter_list,filter_info$filt,filter_info$Filt,filter_info$info,info,'int1',int1)
-plot_venn2<-plot_venn(f_list2,info,'int1',int1)
 int2<-make_list(f_list2)
 #third venn allele
 filter_list<-c('allele','miss','het','GQfiltered')
 f_list3<-run_filter(filter_list,filter_info$filt,filter_info$Filt,filter_info$info,info,'int2',int2)
-plot_venn3<-plot_venn(f_list3,info,'int2',int2)
 int3<-make_list(f_list3)
 list_final<-colsplit(int3,'.1_',c('CHROM','POS'))
 list_final$CHROM<-paste0(list_final$CHROM,'.1')
@@ -121,3 +132,16 @@ list$NC<-NULL
 print('count VCF allele and biological filters')
 print(data.table(table(list$CHROM)))
 print(length(int3))
+
+#first venn (mapping quality filter)
+filter_list<-c('FS','SOR','MQ','MQRankSum','ReadPosRankSum')
+name_f1<-name_filter(filter_list,filter_info$filt,filter_info$Filt,filter_info$info,info,'none',NA)
+plot_venn1<-plot_venn(filter_list,f_list1,name_f1,'none',NA)
+#second venn quality
+filter_list<-c('QUAL','QD')
+name_f2<-name_filter(filter_list,filter_info$filt,filter_info$Filt,filter_info$info,info,'int1',int1)
+plot_venn2<-plot_venn(filter_list,f_list2,name_f2,'int1',int1)
+#third venn allele
+filter_list<-c('allele','miss','het','GQfiltered')
+name_f3<-name_filter(filter_list,filter_info$filt,filter_info$Filt,filter_info$info,info,'int2',int2)
+plot_venn3<-plot_venn(filter_list,f_list3,name_f3,'int2',int2)
